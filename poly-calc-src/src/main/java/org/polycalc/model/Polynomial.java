5876185358435432091e5bc99417f8034422c571
Jawad-af <aboufakherj@gmail.com>
2024-03-19 07:27:28 +0200

    committing the final modifications of the code after testing, in order to start enhancing the readability of the code and its structure

--- poly-calc-src/src/main/java/org/polycalc/model/Polynomial.java
+++ poly-calc-src/src/main/java/org/polycalc/model/Polynomial.java
@@ -19,37 +19,6 @@ public class Polynomial {
         return terms;
     }
 
-    public Variable getVariable() {
-        return Monomial.var;
-    }
-
-    public void print() {
-
-        Iterator<Map.Entry<Integer, Monomial>> iterator = terms.entrySet().iterator();
-        Variable variable = getVariable();
-
-        while (iterator.hasNext()) {
-            Map.Entry entry = iterator.next();
-            Monomial monomial = (Monomial) entry.getValue();
-            double coeff = monomial.getCoeff();
-            int expo = monomial.getExpo();
-
-            if (coeff == 0) {
-                continue;
-            } else if (coeff == 1 && expo != 0) {
-                System.out.print(variable.getName() + SuperScript.convert(expo));
-            } else if (expo == 0) {
-                System.out.print(coeff);
-            } else if (expo == 1) {
-                System.out.print( coeff + variable.getName());
-            } else {
-                System.out.print( coeff + variable.getName() + SuperScript.convert(expo));
-            }
-            if (iterator.hasNext()) {
-                System.out.print(" + ");
-            }
-        }
-    }
 }
 
 
